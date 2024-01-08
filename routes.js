588cf347a3cd7a41cc92a32b304e0467db4737fe
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-01-08 22:27:04 +0200

    added the post in the route

--- routes.js
+++ routes.js
@@ -8,3 +8,17 @@ router.get("/posts", async (req, res) => {
 });
 
 module.exports = router;
+
+router.post("/posts", async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  await post.save();
+  res.send(post);
+});
+
+router.get("/posts/:id", async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.send(post);
+});
