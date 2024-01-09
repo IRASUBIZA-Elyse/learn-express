6b3aabd8a772ae99f4812f897f536fd4c4dcdc7f
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-01-09 20:19:33 +0200

    added the content on the post with save and delete option

--- routes.js
+++ routes.js
@@ -22,3 +22,43 @@ router.get("/posts/:id", async (req, res) => {
   const post = await Post.findOne({ _id: req.params.id });
   res.send(post);
 });
+
+router.get("/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.id });
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+router.patch("/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.id });
+
+    if (req.body.title) {
+      post.title = req.body.title;
+    }
+
+    if (req.body.content) {
+      post.content = req.body.content;
+    }
+
+    await post.save();
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+router.delete("/posts/:id", async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.status(204).send();
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
